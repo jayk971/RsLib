fda9a3ab3e150089bff30ee088416b164b8731ed
ryann chen <ryann chen@ft>
2022-10-11 12:30:46 +0800

    tmp done

---
+++ FT.Common/FT.Common/Enum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RsLib.Common
+{
+    public enum TimeOutType : int
+    {
+        TimeOut = -2,
+        InTime = -1,
+    }
+}
