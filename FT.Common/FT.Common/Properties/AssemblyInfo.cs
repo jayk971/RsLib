6bb912f24bc2ed9b46f82affe159e14b71a9750d
Ryann chen <Ryann chen@FT>
2023-02-23 10:05:36 +0800

    Rebuild

--- FT.Common/FT.Common/Properties/AssemblyInfo.cs
+++ FT.Common/FT.Common/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // ?]???w?]?A?p?U???:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.23052.24")]
+[assembly: AssemblyFileVersion("3.0.23054.0")]
