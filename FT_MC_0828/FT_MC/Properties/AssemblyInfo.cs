1e6c1ffaaf2cc4c0621d5d2ee37fdce8f4d2e968
Ryann chen <Ryann chen@FT>
2023-06-29 10:55:42 +0800

    rebuild all

--- FT_MC_0828/FT_MC/Properties/AssemblyInfo.cs
+++ FT_MC_0828/FT_MC/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // ???w???w?]??:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("3.0.23180.8")]
+[assembly: AssemblyFileVersion("3.0.23180.9")]
