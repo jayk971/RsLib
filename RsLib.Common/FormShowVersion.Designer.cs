9286baa586c8356bb94c9807952aa7c70cbea7c5
Ryann chen <Ryann chen@FT>
2023-08-08 11:17:54 +0800

    hide version form's max and min button adjust version form's size

--- RsLib.Common/FormShowVersion.Designer.cs
+++ RsLib.Common/FormShowVersion.Designer.cs
@@ -36,8 +36,10 @@ namespace RsLib.Common
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 520);
+            this.ClientSize = new System.Drawing.Size(394, 520);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormShowVersion";
             this.Text = "File Version";
             this.ResumeLayout(false);
