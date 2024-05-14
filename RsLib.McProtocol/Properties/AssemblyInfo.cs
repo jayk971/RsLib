a74db3ff88f88d7242e84b0a576e7e6366855b16
Ryann chen <Ryann chen@FT>
2024-05-14 08:12:32 +0800

    temp commit

--- RsLib.McProtocol/Properties/AssemblyInfo.cs
+++ RsLib.McProtocol/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // ???w???w?]??:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("3.0.24120.0")]
+[assembly: AssemblyFileVersion("3.0.24127.0")]
